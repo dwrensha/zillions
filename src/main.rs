8e90580489b29ccf0d8b1cd03689d75d59a51c51
David Renshaw <david@sandstorm.io>
2016-09-25 19:41:02 -0400

    fill in more ReadStream stuff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[macro_use]
 extern crate futures;
+
+#[macro_use]
 extern crate tokio_core;
 
 use std::env;
@@ -7,15 +9,26 @@ use std::net::SocketAddr;
 
 use futures::{Async, Poll, Future};
 use futures::stream::Stream;
-use tokio_core::io::{read_exact, Io};
+use tokio_core::io::{read_exact};
 use tokio_core::net::TcpListener;
 use tokio_core::reactor::Core;
 
 struct ReadStream<R> where R: ::std::io::Read {
     reader: R,
-    buffer: [u8; 256],
+    buffer: Vec<u8>,
     pos: usize,
-    frame_end: u8,
+    frame_end: Option<u8>,
+}
+
+impl <R> ReadStream<R> where R: ::std::io::Read {
+    fn new(reader: R) -> ReadStream<R> {
+        ReadStream {
+            reader: reader,
+            buffer: Vec::new(),
+            pos: 0,
+            frame_end: None,
+        }
+    }
 }
 
 impl <R> Stream for ReadStream<R> where R: ::std::io::Read {
@@ -23,7 +36,26 @@ impl <R> Stream for ReadStream<R> where R: ::std::io::Read {
     type Error = ::std::io::Error;
 
     fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        unimplemented!()
+        loop {
+            if let Some(frame_end) = self.frame_end {
+                let n = try_nb!(self.reader.read(&mut self.buffer[self.pos..frame_end as usize]));
+                self.pos += n;
+                if self.pos == frame_end as usize {
+                    self.pos = 0;
+                    let result = ::std::mem::replace(&mut self.buffer, Vec::new());
+                    return Ok(Async::Ready(Some(result)))
+                }
+            } else {
+                let mut buf = [0u8];
+                let n = try_nb!(self.reader.read(&mut buf));
+                if n == 0 { // EOF
+                    return Ok(Async::Ready(None))
+                }
+
+                self.frame_end = Some(buf[0]);
+                self.buffer = vec![0; buf[0] as usize];
+            }
+        }
     }
 }
 
@@ -40,29 +72,36 @@ pub fn main() {
     // Once we've got the TCP listener, inform that we have it
     println!("Listening on: {}", addr);
 
-    let done = socket.incoming().for_each(move |(socket, addr)| {
+    let done = socket.incoming().for_each(move |(socket, _addr)| {
         // what's the spec?
         // first byte: 0 means publisher, 1 means subscriber.
 
-        let mut header = [0u8; 1];
+        let header = [0u8; 1];
         let future = read_exact(socket, header).and_then(|(socket, header)| {
+            println!("OK {:?}", header);
             match header[0] {
                 0 => {
                     // publisher
-                    // hm... is this framing going to be tricky?
+                    let done = ReadStream::new(socket).for_each(|buf| {
+                        println!("buf {:?}", buf);
+                        Ok(())
+                    });
+
+                    // When this stream is done, I want the socket back.
+                    done
+
                     // When done receiving message, write back the number received,
                     // as a little-endian u64.
                 }
                 1 => {
                     //subscriber
+                    unimplemented!()
                 }
                 _ => {
                     // error
+                    unimplemented!()
                 }
             }
-
-            println!("OK {:?}", header);
-            Ok(())
         }).map_err(|e| {
             println!("error: {}", e);
         });
