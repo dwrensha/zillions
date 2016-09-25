889666553da87593c99a636ebd97a4e547dd530e
David Renshaw <david@sandstorm.io>
2016-09-25 18:48:55 -0400

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
+#[macro_use]
 extern crate futures;
 extern crate tokio_core;
 
 use std::env;
 use std::net::SocketAddr;
 
-use futures::Future;
+use futures::{Async, Poll, Future};
 use futures::stream::Stream;
 use tokio_core::io::{read_exact, Io};
 use tokio_core::net::TcpListener;
 use tokio_core::reactor::Core;
 
+struct ReadStream<R> where R: ::std::io::Read {
+    reader: R,
+    buffer: [u8; 256],
+    pos: usize,
+    frame_end: u8,
+}
 
-// Should listen on the TCP address passed in.
+impl <R> Stream for ReadStream<R> where R: ::std::io::Read {
+    type Item = Vec<u8>;
+    type Error = ::std::io::Error;
+
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        unimplemented!()
+    }
+}
 
 pub fn main() {
     let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
@@ -29,10 +43,24 @@ pub fn main() {
     let done = socket.incoming().for_each(move |(socket, addr)| {
         // what's the spec?
         // first byte: 0 means publisher, 1 means subscriber.
-        // second byte: the channel ID.
 
-        let mut header = [0u8; 2];
+        let mut header = [0u8; 1];
         let future = read_exact(socket, header).and_then(|(socket, header)| {
+            match header[0] {
+                0 => {
+                    // publisher
+                    // hm... is this framing going to be tricky?
+                    // When done receiving message, write back the number received,
+                    // as a little-endian u64.
+                }
+                1 => {
+                    //subscriber
+                }
+                _ => {
+                    // error
+                }
+            }
+
             println!("OK {:?}", header);
             Ok(())
         }).map_err(|e| {
