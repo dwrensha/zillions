ff85205c6a482ae302d9d07f2daf63e76f747c3d
David Renshaw <david@sandstorm.io>
2016-10-02 22:55:53 -0400

    publisher half

--- impls/gjio/server.rs
+++ impls/gjio/server.rs
@@ -3,16 +3,56 @@ extern crate gjio;
 extern crate byteorder;
 extern crate slab;
 
-use gj::{EventLoop, Promise, PromiseFulfiller, TaskReaper, TaskSet};
+use std::io::{Error, ErrorKind};
+use byteorder::{LittleEndian, ByteOrder};
+use gj::{EventLoop, Promise, TaskReaper, TaskSet};
+use gjio::{SocketStream, AsyncRead, AsyncWrite};
+
+fn handle_publisher(mut stream: SocketStream, messages_received: u64) -> Promise<(), Error> {
+    stream.try_read(vec![0], 1).then(move |(buf, n)| {
+        if n == 0 {
+            // EOF
+            let mut word = vec![0u8; 8];
+            <LittleEndian as ByteOrder>::write_u64(&mut word, messages_received);
+            stream.write(word).map(|_| Ok(()))
+        } else {
+            let len = buf[0] as usize;
+            let body = vec![0u8; len];
+            stream.read(body, len).then(move |(buf, _)| {
+                // TODO send buf to subscribers
+                handle_publisher(stream, messages_received + 1)
+            })
+        }
+    })
+}
+
+
+fn handle_connection(mut stream: SocketStream) -> Promise<(), Error> {
+    stream.read(vec![0], 1).then(move |(buf, _)| {
+        match buf[0] {
+            0 => {
+                // publisher
+                handle_publisher(stream, 0)
+            }
+            1 => {
+                // subscriber
+                unimplemented!()
+            }
+            _ => {
+                Promise::err(Error::new(ErrorKind::Other, "expected 0 or 1"))
+            }
+        }
+    })
+}
 
 fn accept_loop(listener: gjio::SocketListener,
                mut task_set: TaskSet<(), ::std::io::Error>)
                -> Promise<(), ::std::io::Error>
 {
-    unimplemented!()
-//    listener.accept().then(move |stream| {
-//        accept_loop(listener, task_set)
-//    })
+     listener.accept().then(move |stream| {
+         task_set.add(handle_connection(stream));
+         accept_loop(listener, task_set)
+    })
 }
 
 struct Reaper;
